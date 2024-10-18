3f8ec5f89f8f379a84489418b7a79b865e144077
valikriabinchak <riabinchak.valentyn@student.uzhnu.edu.ua>
2024-10-18 19:03:50 +0300

    Added husky git hooks to github. Corrected lint-staged and eslint configs

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    '*.{js,jsx}': ['eslint --ignore-path .gitignore --fix --ext .js,.jsx --max-warnings 0'],
-    '*.{js,jsx,css,mjs}': ['prettier --write'],
+    '*.{js,jsx}': [ 'eslint --ignore-path .gitignore --fix --ext .js,.jsx --max-warnings 0 ./src' ],
+    '*.{js,jsx,css,mjs}': [ 'prettier --write' ],
 }
