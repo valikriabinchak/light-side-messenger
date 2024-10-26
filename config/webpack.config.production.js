7d8e845ddb627997fc5f9d5659c16ff35a79ce50
valikriabinchak <riabinchak.valentyn@student.uzhnu.edu.ua>
2024-10-26 22:29:35 +0300

    Updated WebPack production

--- config/webpack.config.production.js
+++ config/webpack.config.production.js
@@ -3,10 +3,35 @@ const baseConfig = require( './webpack.config' );
 
 const config = {
     mode: "production",
-    module: {
-        rules: [ { test: /\.txt$/, use: 'raw-loader' } ],
+    output: {
+        path: path.resolve( __dirname, '../dist' ),
+        filename: 'index.js',
     },
-    plugins: [ new HtmlWebpackPlugin( { template: './src/index.html' } ) ],
+    plugins: [ new HtmlWebpackPlugin( {
+        template: path.resolve( __dirname, '../templates/index.html' ), // Absolute path
+        filename: 'index.html',
+        title: 'My App',
+    } ) ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /\.(js|jsx)$/,
+                use: [ {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [ '@babel/preset-env' ]
+                    }
+                } ], // Webpack process loaders from the end to the start
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.js', '.jsx' ], // Allow importing without specifying extensions
+    },
 };
 
 module.exports = merge( baseConfig, config );
