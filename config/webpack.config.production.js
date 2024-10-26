b38512b44d3362150db4c35945640cb7f88e670f
valikriabinchak <riabinchak.valentyn@student.uzhnu.edu.ua>
2024-10-26 22:29:03 +0300

    WebPack production

--- config/webpack.config.production.js
+++ config/webpack.config.production.js
@@ -1,8 +1,12 @@
-const { merge } = require('webpack-merge');
-const baseConfig = require('./webpack.config');
+const { merge } = require( 'webpack-merge' );
+const baseConfig = require( './webpack.config' );
 
 const config = {
     mode: "production",
+    module: {
+        rules: [ { test: /\.txt$/, use: 'raw-loader' } ],
+    },
+    plugins: [ new HtmlWebpackPlugin( { template: './src/index.html' } ) ],
 };
 
-module.exports = merge(baseConfig, config);
+module.exports = merge( baseConfig, config );
